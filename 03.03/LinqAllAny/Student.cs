519096c4cacc42429f57cb6e4580786ef4abe721
Opilane TTHK <opilane@tthk.ee>
2026-03-03 15:53:12 +0200

    03.03

---
+++ 03.03/LinqAllAny/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqAllAny
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int StandartId { get; set; }
+    }
+}
---
+++ 03.03/LinqAllAny/StudentData.cs
@@ -0,0 +1,14 @@
+﻿namespace LinqAllAny
+{
+    public class StudentData
+    {
+        public static readonly List<Student> students = new List<Student>()
+        {
+            new Student() {StudentId  = 1, Name = "John", Age = 18},
+            new Student() {StudentId  = 2, Name = "Moin", Age = 21},
+            new Student() {StudentId  = 3, Name = "Bill", Age = 18},
+            new Student() {StudentId  = 4, Name = "Ram", Age = 20},
+            new Student() {StudentId  = 5, Name = "Ron", Age = 15},
+        };
+    }
+}
