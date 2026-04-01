c60a5a4efaad68f70244aa430126f83ed2c1594e
Opilane TTHK <mark.tjuska1@gmail.com>
2026-04-01 11:35:15 +0300

    LayoutIndex

---
+++ 01.04/University/University/Models/Enrollment.cs
@@ -0,0 +1,16 @@
+﻿namespace University.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int CourdeId { get; set; }
+        public int StudentId { get; set; }
+        public Grade? Grade { get; set; }
+        public Course Course 
+    }
+
+    public enum Grade
+    {
+        A, B ,C, D, F
+    }
+}
